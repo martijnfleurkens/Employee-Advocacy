51cb301e4d6e962699cab51c7b29ef7ddff6f2ab
martijnfl <martijnfleurkens@gmail.com>
2020-03-23 12:01:06 +0100

    First commit of the first prototype

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div id="focus_wrapper">
+    <router-view></router-view>
+    <img src="./assets/valtech_logo.png" alt="Logo" class="logo">
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
 }
 </script>
 
@@ -23,6 +21,51 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  position: absolute;
+  width: 100%;
+  height: 100%;
+}
+
+#focus_wrapper{
+  background: #F6F6F6;
+  width: 650px;
+  margin: auto;
+  height: 100%;
+  position: relative;
+}
+
+#focus_wrapper .logo{
+  height: 40px;
+  position: relative;
+  bottom: -100px;
+  margin: auto;
+}
+
+html, 
+body {
+    height: 100%;
+    margin: 0px;
+}
+
+
+.btn{
+  font-size: 16px;
+    background-color: black;
+    color: white;
+    border: none;
+    padding: 15px 60px;
+    cursor: pointer;
+    transition: 0.5s all ease-in-out;
+    font-weight: bold; 
+}
+
+.btn:hover{
+    background-color: #00C8FF;
+    transition: 0.3s all ease-in-out;
+}
+
+.btn.center{
+  display: table;
+  margin: auto;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,63 @@
 import Vue from 'vue'
+
+import VueRouter from 'vue-router'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+import VueAuthenticate from 'vue-authenticate'
+
+//Templates
 import App from './App.vue'
+
+  //routes
+  import Post from './Post.vue'
+  import Activate from './Activate.vue'
+  import Thank_you from './Thank_you.vue'
 
 Vue.config.productionTip = false
 
+//Vue router
+Vue.use(VueRouter);
+const routes = [
+  { path: '/activate', component: Activate },
+  { path: '/', component: Post },
+  { path: '/thank_you', component: Thank_you },
+];
+
+const router = new VueRouter({
+  routes,
+  mode: 'history',
+});
+
+//Axios
+Vue.use(VueAxios, axios);
+
+//VueAuthenticate
+Vue.use(VueAuthenticate, {
+  // baseUrl: 'http://localhost:8080', // Your API domain
+  baseUrl: 'http://server.martijnfl.nl', // Your API domain
+  tokenName: 'access_token',
+  tokenPath: 'access_token',
+  
+  providers: {
+    linkedin: {
+      clientId: '86h220n2vlk99q',
+      redirectUri: 'http://localhost:8080/activate', // Your client app URL
+      // redirectUri: 'http://server.martijnfl.nl/test/api.php', // Your client app URL
+      state: function () { 
+        return '!o!HH-Q991dF';//new Date().getTime().toString();
+      },
+      // url: 'http://localhost:8080/test/api.php',
+      url: '/test/api.php',
+      scope:['r_liteprofile','r_emailaddress','w_member_social'],
+      // optionalUrlParams: ['scope', 'state', 'url']
+      optionalUrlParams: ['scope', 'state']
+    }
+  }
+})
+
+//Start Vue
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  el: '#app',
+  router,
+  render: h => h(App)
+});
